c566f5f51febab9cfabc18c7fe31012d44fe87fc
mibastid <mibastid@gmail.com>
2017-03-22 17:00:00 +0100

    Arreglando el parser lrc, a falta del cálculo de duraciones

--- parserLrc.php
+++ parserLrc.php
@@ -9,34 +9,36 @@ $songName = explode('.lrc', $filename)[0];
 $lines = [];
 
 if(!feof($inputFile)) {
-	$txt = utf8_encode(fgets($inputFile));
+	$txt = fgets($inputFile);
 }
 
 while(!feof($inputFile)) {
 	$line = [];
 	$aux = [];
-	while((strcmp($txt[0],'-')==0) && !feof($inputFile)){
+	while(!(strpos($txt, "\\n") !== false) && !feof($inputFile)){
 		$syllable = createSyllable($txt);
 		array_push($aux, $syllable);
 		$txt = fgets($inputFile);
 	}
 
+	$syllable = createSyllable($txt);
+	$syllable['value']=substr($syllable['value'], 0, -2);
+	array_push($aux, $syllable);
+
+	$aux = getDurationSyllable($aux, $txt);
+
 	if(count($aux)>0){
 		$line['syllables'] = $aux;
 		array_push($lines, $line);
 	}
-	$txt = utf8_encode(fgets($inputFile));
-	
-	
+	$txt = fgets($inputFile);
 }
-
-
 
 $song = [];
 $song['song'] = $songName;
 $song['lines'] = $lines;
 print_r(json_encode($song,JSON_PRETTY_PRINT));
-$outputFile = fopen($songName."_ultra.json", "w");
+$outputFile = fopen($songName."_lrc.json", "w");
 
 fwrite($outputFile, json_encode($song,JSON_PRETTY_PRINT));
 
@@ -46,21 +48,24 @@ fclose($outputFile);
 echo '<br>fin';
 
 function createSyllable($textLine){
-	$aux = explode(" ", $textLine);
+	$aux = explode("-", $textLine);
 	$syllable = [];
-	$syllable['initTimeBeat']= $aux[1];
-	$syllable['durationBeat']=$aux[2];
-	$syllable['note']=$aux[3];
-	$syllable['value']=utf8_encode($aux[4]);
-	$i = 5;
-	while($i < count($aux)){
-		$syllable['value'] = $syllable['value'] . ' ' . utf8_encode($aux[ $i ]);
-		$i++;
+	$syllable['initTime']= $aux[0];
+	$syllable['duration']=0;
+	$syllable['value']=$aux[2];
+	if(count($aux)==4){
+		$syllable['value']= $syllable['value'] . '-';
+	}else{
+		$syllable['value']=substr($syllable['value'], 0, -1);
 	}
-	$syllable['value']=substr($syllable['value'], 0, -2);
-
 	return $syllable;
 }
+
+function getDurationSyllable($syllables, $lastLine){
+	$i = 0;
+	$j = 0;
+}
+
 
 
 ?>
